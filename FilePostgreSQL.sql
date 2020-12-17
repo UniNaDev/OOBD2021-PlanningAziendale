f56923d43a73efd71de2f99951bc7f6e1da1c65c
MicheleZurlo01 <75538307+MicheleZurlo01@users.noreply.github.com>
2020-12-17 17:57:39 +0100

    UPDATE: FilePostgreSQL
    Aggiunta dei check di validità.
    Check per validità Nome, Cognome, CF, Email nella tabella Dipendente.
    Aggiunta di alcune interrogazioni.
    Aggiunta di dati alle tabelle.

--- FilePostgreSQL.sql
+++ FilePostgreSQL.sql
@@ -33,9 +33,9 @@ CREATE TABLE Progetto (
 	TipoProgetto tipologia ,
 	DescrizioneProgetto varchar (200),
 	DataCreazione DATE NOT NULL,
-	Scadenza DATE,
+	DataScadenza DATE,
 	DataTerminazione DATE,
-	Creatore char(40),
+	Creatore char(40) NOT NULL,
 	
 	PRIMARY KEY(CodProgetto)
 );
@@ -62,23 +62,26 @@ CREATE TABLE LuogoNascita(
 
 
 CREATE TABLE Dipendente(
-	CF char(16),
+	CF char(16) CHECK(CF ~* '^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$'),
 	Nome varchar(20) NOT NULL,
 	Cognome varchar(20) NOT NULL,
 	DataNascita DATE NOT NULL,
 	Indirizzo varchar(100) NOT NULL,
-	Email varchar(100) NOT NULL,
-	TelefonoCasa integer,
-	Cellulare integer,
+	Email varchar(100) CHECK(Email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
+	TelefonoCasa char(10),
+	Cellulare char(10),
 	Salario integer NOT NULL,
 	Valutazione integer NOT NULL,
 	Password varchar(50) NOT NULL,
 	CodComune char(20) NOT NULL, 
+
 	
 	PRIMARY KEY(CF),
 	UNIQUE(Email),
 	UNIQUE(TelefonoCasa),
 	UNIQUE(Cellulare),
+	CHECK(Nome ~* '^[ a-zA-Z]+$'),
+	CHECK(Cognome ~* '^[ a-zA-Z]+$'),
 	
 	--Associazione 1 a Molti(LuogoNascita,Dipendente)
 	FOREIGN KEY (CodComune) REFERENCES LuogoNascita(CodComune)
@@ -146,7 +149,7 @@ CREATE TYPE ruolo AS ENUM('Project Manager','Team Member','Team Leader','Chief F
 
 CREATE TABLE Partecipazione(
 	CodProgetto integer,
-	CF char(16),
+	CF char(16) CHECK(CF ~* '^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$'),
 	RuoloDipendente ruolo,
 	
 	FOREIGN KEY (CodProgetto) REFERENCES Progetto(CodProgetto),
@@ -157,7 +160,7 @@ CREATE TABLE Partecipazione(
 --Associazione Molti a Molti (Dipendente,Skill)
 CREATE TABLE Abilità(
 	IDSkill integer,
-	CF char(16),
+	CF char(16) CHECK(CF ~* '^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$'),
 	
 	FOREIGN KEY (IDSkill) REFERENCES Skill(IDSkill),
 	FOREIGN KEY (CF) REFERENCES Dipendente(CF)
@@ -166,7 +169,7 @@ CREATE TABLE Abilità(
 
 --Associazione Molti a Molti (Meeting,Dipendente)
 CREATE TABLE Presenza(
-	CF char(16),
+	CF char(16) CHECK(CF ~* '^[A-Z]{6}[0-9]{2}[A-Z][0-9]{2}[A-Z][0-9]{3}[A-Z]$'),
 	IDMeeting integer,
 	
 	FOREIGN KEY (CF) REFERENCES Dipendente(CF),
@@ -179,11 +182,13 @@ CREATE TABLE Presenza(
 
 /* Inserimento Dati */
 
+
 --Progetto
 INSERT INTO Progetto(CodProgetto,NomeProgetto,TipoProgetto,DescrizioneProgetto,DataCreazione,DataScadenza,DataTerminazione,Creatore) VALUES
 	(1,'Mio Progetto','Ricerca base','dffgffgff','16/12/2020',null,null,'Michekle'),
 	(2,'MibProgo','Sviluppo sperimentale','dffgffbbgff','16/12/2020',null,null,'Micheggkle'),
 	(3,'MixccProgetto','Ricerca sperimentale','dffgffffgff','16/12/2020',null,null,'Micheklfe');
+
 
 --AmbitoProgetto
 INSERT INTO AmbitoProgetto(IDAmbito,NomeAmbito) VALUES
@@ -191,23 +196,13 @@ INSERT INTO AmbitoProgetto(IDAmbito,NomeAmbito) VALUES
 	(2,'Medicina'),
 	(3,'Militare'),
 	(4,'Scientifico');
-	
-	
---
+
 
 --Dipendente
-INSERT INTO Dipendente(CF,Nome,Cognome,DataNascita,Indirizzo,Email,TelefonoCasa,Cellulare,Salario,Valutazione,Password) VALUES
-	('ZDDFCCD34544','Mi','mmmSss','11/12/2000','via sdff,28','sjdndjnsj@ff.com','08175898','38789999',10000,10,'pass'),
-	('ZDDFCCD34554544','Mcci','nSss','17/12/2000','via sdffcc,27','sjdndjnffdsj@ff.com','08175755','387719999',50000,1,'paddss'),
-	('ZDDFCCD3454544','Mivv','bnSss','19/12/2000','via sdff,38','sjdndcccdfjnsj@ff.com','0817589895','3448999',100000,5,'passw');
-
-
---Partecipazione
-INSERT INTO Partecipazione(CodProgetto,CF,RuoloDipendente) VALUES
-	(1,null,'Project Manager'),
-	(2,null,'Team Member');
-
-
+INSERT INTO Dipendente(CF,Nome,Cognome,DataNascita,Indirizzo,Email,TelefonoCasa,Cellulare,Salario,Valutazione,Password,CodComune) VALUES
+	('RSSMRA80A01F839W','Mario','Rossi','01/01/1980','via sdff,28','sjdndjnsj@ff.com','0817589891','38789999',10000,10,'pass','F839'),
+	('DLCGPP80L01H243P','Giuseppe','De Lucia','01/07/1980','via sdffcc,27','sj545ffd@sjff2548.com','0817327550','3877199990',50000,1,'paddss','H243'),
+	('SPSNDR02L01L259V','Andrea','Esposito','01/07/2002','via sdff,38','sjdndcccdfjnsj@ff.com','0817589895','3448999000',100000,5,'passw','L259');
 
 
 --Skill
@@ -233,6 +228,43 @@ INSERT INTO Meeting(IDMeeting,DataInizio,DataFine,OrarioInizio,OrarioFine,Modali
 	(3,'12/10/2020',null,'13:55','14:55','Fisico',null,'Micchddd','Sala2');
 	
 
-	
+--AmbitoProgettoLink
+INSERT INTO AmbitoProgettoLink(IDAmbito,CodProgetto) VALUES
+	(1,1),
+	(2,2),
+	(3,3);
 
+
+--Partecipazione (Trigger:Non possono essere inseriti più Project Manager per lo stesso progetto)
+INSERT INTO Partecipazione(CodProgetto,CF,RuoloDipendente) VALUES
+	(1,'RSSMRA80A01F839W','Project Manager'),
+	(1,'DLCGPP80L01H243P','Project Manager'),
+	(1,'RSSMRA80A01F839W','Project Manager'),
+	(2,'RSSMRA80A01F839W','Team Member');
+	
+	
+--Abilità
+INSERT INTO Abilità(IDSkill,CF) VALUES
+	(10,'RSSMRA80A01F839W'),
+	(1,'DLCGPP80L01H243P'),
+	(100,'SPSNDR02L01L259V');
+
+
+--Presenza
+INSERT INTO Presenza(CF,IDMeeting) VALUES
+	('RSSMRA80A01F839W',1),
+	('DLCGPP80L01H243P',2),
+	('SPSNDR02L01L259V',3);
+
+/* --Interrogazioni-- */
+
+--Dipendenti che hanno partecipato a dei progetti
+SELECT *
+FROM Partecipazione NATURAL JOIN Dipendente
+
+
+--Dipendenti che hanno partecipato ai meeting
+SELECT IDMeeting,CF,Nome,Cognome,valutazione
+FROM Dipendente NATURAL JOIN Presenza
+ORDER BY valutazione DESC
 
