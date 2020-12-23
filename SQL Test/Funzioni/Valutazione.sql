e6164dafb34259bf2c1d1338466924755fba043e
Andrea Lemmo <75537622+AndreaLemmo@users.noreply.github.com>
2020-12-23 18:15:38 +0100

    Aggiunta funzione per calcolo Valutazione
    Aggiunta funzione per il calcolo della valutazione di un dipendente in funzione delle presenze ai meeting e delle partecipazioni ai progetti.  Presenti anche le funzioni intermedie per il calcolo di solo le valutazioni rispetto a uno o l'altro criterio.

---
+++ SQL Test/Funzioni/Valutazione.sql
@@ -0,0 +1,72 @@
+/*FUNCTION PER VALUTAZIONE DIPENDENTE
+**La funzione calcola la valutazione del dipendente in decimi
+**basandosi sulla sua partecipazione ai progetti e al suo numero
+**di presenze ai meeting.
+*****************************************************************/
+
+--FUNCTION PER VALUTAZIONE MEETING
+CREATE OR REPLACE FUNCTION ValutazioneMeeting(dip Dipendente.CF%TYPE) RETURNS FLOAT
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+ValutazioneMeeting FLOAT;
+TotaleMeeting FLOAT;
+Presenze FLOAT;
+BEGIN
+--Conta tutti i meeting a cui doveva partecipare
+SELECT COUNT(*) INTO TotaleMeeting
+FROM Presenza AS p
+WHERE p.CF = dip;
+--Conta le sue presenze per ciascuno di essi
+SELECT COUNT(*) INTO Presenze
+FROM Presenza AS p
+WHERE p.CF=dip AND p.Presente=TRUE;
+--Fai il rapporto e restituisci il valore in decimi
+ValutazioneMeeting := (Presenze*10)/TotaleMeeting;
+RETURN ValutazioneMeeting;
+END;
+$$;
+----------------------------------------------------------------------------------------
+
+--FUNCTION PER PROGETTI
+CREATE OR REPLACE FUNCTION ValutazioneProgetti(dip Dipendente.CF%TYPE) RETURNS FLOAT
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+TotaleProgetti FLOAT;
+Partecipazioni FLOAT;
+ValutazioneProgetti FLOAT;
+BEGIN
+--Calcola il numero totale di progetti dell'azienda
+SELECT COUNT(*) INTO TotaleProgetti
+FROM Progetto;
+--Calcola il numero di progetti a cui il dipendente partecipa
+SELECT COUNT(*) INTO Partecipazioni
+FROM Partecipazione AS p
+WHERE p.CF = dip;
+--Fai il rapporto e restituisci il risultato
+ValutazioneProgetti := (Partecipazioni*10)/TotaleProgetti;
+RETURN ValutazioneProgetti;
+END;
+$$;
+-----------------------------------------------------------------------------------------
+
+--FUNCTION PRINCIPALE
+CREATE OR REPLACE FUNCTION Valutazione(dip Dipendente.CF%TYPE) RETURNS FLOAT
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+ValMeeting FLOAT;
+ValProgetti FLOAT;
+ValutazioneFinale FLOAT;
+BEGIN
+--Calcola valutazione meeting
+ValMeeting := ValutazioneMeeting(dip);
+--Calcola valutazione progetti
+ValProgetti := ValutazioneProgetti(dip);
+--Fai la media e restituisci il risultato in decimi
+ValutazioneFinale := (ValMeeting+ValProgetti)/2;
+RETURN ValutazioneFinale;
+END;
+$$;
+--------------------------------------------------------------------------------------------------------------------------
