b169744b68104f199bfbbf79106ea4dc6c7776d7
Andrea Lemmo <75537622+AndreaLemmo@users.noreply.github.com>
2021-01-03 13:35:19 +0100

    Modificato CONSTRAINT OrarioValidoMeeting
    Modificato constraint OrarioValidoMeeting. Ora contempla anche la possibilità che il meeting avvenga in date diverse e con orario di inizio > orario di fine

--- FILE SQL/FilePostgreSQL.sql
+++ FILE SQL/FilePostgreSQL.sql
@@ -133,7 +133,7 @@ CREATE TABLE Meeting(
 	
 	PRIMARY KEY(IDMeeting),
 	CONSTRAINT DataValidaMeeting CHECK(DataInizio <= DataFine),
-	CONSTRAINT OrarioValidoMeeting CHECK(OrarioInizio < OrarioFine),
+	CONSTRAINT OrarioValidoMeeting CHECK(OrarioInizio < OrarioFine AND DataInizio = DataFine),
 	CONSTRAINT ModalitàRiunione CHECK ((Modalità='Telematico' AND Piattaforma IS NOT NULL AND CodSala IS NULL) OR (Modalità = 'Fisico' AND Piattaforma IS NULL AND CodSala IS NOT NULL)),
 	
 	
