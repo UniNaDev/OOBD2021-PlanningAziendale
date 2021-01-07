073b513dcfe594dc991e54318a91688684e400d7
daddi99 <d.soldatini@studenti.unina.it>
2021-01-07 12:43:58 +0100

    Update I Miei Progetti
    -Aggiunte Icone e highlight dei bottoni
    -Cambiato nomi variabili

--- UniNaDevProject/src/GUI/Project.java
+++ UniNaDevProject/src/GUI/Project.java
@@ -34,25 +34,45 @@ import javax.swing.JSeparator;
 import javax.swing.border.MatteBorder;
 import java.awt.Cursor;
 import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Project extends JFrame {
 
 	private JPanel contentPane;
+	private JButton modificaProgettoBotton;
+	private JButton nuovoProgettoBotton;
+	private JButton okButton;
 
 
 	/**
 	 * Create the frame.
 	 */
 	public Project(ControllerProgetto theController) {
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Project.class.getResource("/Icone/WindowIcon_16.png")));
 		setMinimumSize(new Dimension(1200, 700));
-		setTitle("Progetti");
+		setTitle("iPlanner - I miei Progetti");
 		setBounds(100, 100, 1440, 900);
 		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		JButton modificaProgettoBotton = new JButton("Modifica Progetto");
+		modificaProgettoBotton = new JButton("Modifica Progetto");
+		modificaProgettoBotton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) 
+			{
+				modificaProgettoBotton.setBackground(Color.LIGHT_GRAY);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) 
+			{
+				modificaProgettoBotton.setBackground(Color.WHITE);
+			}
+		});
 		modificaProgettoBotton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		modificaProgettoBotton.setBackground(Color.WHITE);
 		modificaProgettoBotton.setBorder(new MatteBorder(1, 1, 1, 1, (Color) Color.LIGHT_GRAY));
@@ -63,7 +83,19 @@ public class Project extends JFrame {
 			}
 		});
 		
-		JButton okButton = new JButton("OK");
+		okButton = new JButton("OK");
+		okButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) 
+			{	
+				okButton.setBackground(Color.LIGHT_GRAY);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) 
+			{
+				okButton.setBackground(Color.WHITE);
+			}
+		});
 		okButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		okButton.setBackground(Color.WHITE);
 		okButton.setBorder(new MatteBorder(1, 1, 1, 1, (Color) Color.LIGHT_GRAY));
@@ -73,7 +105,19 @@ public class Project extends JFrame {
 			}
 		});
 		
-		JButton nuovoProgettoBotton = new JButton("Nuovo Progetto");
+		nuovoProgettoBotton = new JButton("Nuovo Progetto");
+		nuovoProgettoBotton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) 
+			{
+				nuovoProgettoBotton.setBackground(Color.LIGHT_GRAY);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) 
+			{
+				nuovoProgettoBotton.setBackground(Color.WHITE);
+			}
+		});
 		nuovoProgettoBotton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		nuovoProgettoBotton.setBackground(Color.WHITE);
 		nuovoProgettoBotton.setBorder(new MatteBorder(1, 1, 1, 1, (Color) Color.LIGHT_GRAY));
@@ -83,6 +127,7 @@ public class Project extends JFrame {
 		mieiProgettiScrollPane.setBorder(new LineBorder(Color.LIGHT_GRAY, 2));
 		
 		JLabel mieiProgettiLabel = new JLabel("I Miei Progetti");
+		mieiProgettiLabel.setIcon(new ImageIcon(Project.class.getResource("/Icone/progetto_64.png")));
 		mieiProgettiLabel.setFont(new Font("Consolas", Font.PLAIN, 30));
 		
 		JPanel infoProgettoPanel = new JPanel();
@@ -191,38 +236,56 @@ public class Project extends JFrame {
 		JLabel dataCreazioneLabel = new JLabel("Data Creazione:");
 		dataCreazioneLabel.setForeground(Color.DARK_GRAY);
 		dataCreazioneLabel.setFont(new Font("Consolas", Font.PLAIN, 20));
-		dataCreazioneLabel.setBounds(10, 503, 172, 24);
+		dataCreazioneLabel.setBounds(10, 531, 172, 24);
 		infoProgettoPanel.add(dataCreazioneLabel);
 		
 		JLabel dataTerminazioneLabel = new JLabel("Data Terminazione:");
 		dataTerminazioneLabel.setForeground(Color.DARK_GRAY);
 		dataTerminazioneLabel.setFont(new Font("Consolas", Font.PLAIN, 20));
-		dataTerminazioneLabel.setBounds(376, 500, 202, 30);
+		dataTerminazioneLabel.setBounds(376, 528, 202, 30);
 		infoProgettoPanel.add(dataTerminazioneLabel);
 		
 		JLabel dataScadenzaLabel = new JLabel("Data Scadenza:");
 		dataScadenzaLabel.setForeground(Color.DARK_GRAY);
 		dataScadenzaLabel.setFont(new Font("Consolas", Font.PLAIN, 20));
-		dataScadenzaLabel.setBounds(207, 503, 169, 24);
+		dataScadenzaLabel.setBounds(207, 531, 169, 24);
 		infoProgettoPanel.add(dataScadenzaLabel);
 		
-		JLabel lblNewLabel = new JLabel("07/01/2021");
+		JLabel valoreDataCreazioneLabel = new JLabel("07/01/2021");
+		valoreDataCreazioneLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		valoreDataCreazioneLabel.setFont(new Font("Consolas", Font.PLAIN, 22));
+		valoreDataCreazioneLabel.setBounds(10, 565, 161, 24);
+		infoProgettoPanel.add(valoreDataCreazioneLabel);
+		
+		JLabel valoreDataScadenzaLabel = new JLabel("07/01/2022");
+		valoreDataScadenzaLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		valoreDataScadenzaLabel.setFont(new Font("Consolas", Font.PLAIN, 22));
+		valoreDataScadenzaLabel.setBounds(207, 565, 161, 24);
+		infoProgettoPanel.add(valoreDataScadenzaLabel);
+		
+		JLabel valoreDataTerminazioneLabel = new JLabel("N/A");
+		valoreDataTerminazioneLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		valoreDataTerminazioneLabel.setFont(new Font("Consolas", Font.PLAIN, 22));
+		valoreDataTerminazioneLabel.setBounds(386, 569, 161, 24);
+		infoProgettoPanel.add(valoreDataTerminazioneLabel);
+		
+		JLabel lblNewLabel = new JLabel("");
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Consolas", Font.PLAIN, 22));
-		lblNewLabel.setBounds(10, 537, 161, 24);
+		lblNewLabel.setIcon(new ImageIcon(Project.class.getResource("/Icone/dataCreazione_32.png")));
+		lblNewLabel.setBounds(45, 481, 89, 51);
 		infoProgettoPanel.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("07/01/2022");
+		JLabel lblNewLabel_1 = new JLabel("");
+		lblNewLabel_1.setIcon(new ImageIcon(Project.class.getResource("/Icone/dataScadenza_32.png")));
 		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNewLabel_1.setFont(new Font("Consolas", Font.PLAIN, 22));
-		lblNewLabel_1.setBounds(207, 537, 161, 24);
+		lblNewLabel_1.setBounds(239, 481, 89, 51);
 		infoProgettoPanel.add(lblNewLabel_1);
 		
-		JLabel lblNa = new JLabel("N/A");
-		lblNa.setHorizontalAlignment(SwingConstants.CENTER);
-		lblNa.setFont(new Font("Consolas", Font.PLAIN, 22));
-		lblNa.setBounds(386, 541, 161, 24);
-		infoProgettoPanel.add(lblNa);
+		JLabel lblNewLabel_2 = new JLabel("");
+		lblNewLabel_2.setIcon(new ImageIcon(Project.class.getResource("/Icone/dataTerminazione_32.png")));
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setBounds(426, 481, 89, 51);
+		infoProgettoPanel.add(lblNewLabel_2);
 		
 		JList progettiList = new JList();
 		progettiList.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
