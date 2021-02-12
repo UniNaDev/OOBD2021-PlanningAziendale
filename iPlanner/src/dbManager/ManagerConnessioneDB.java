676e2ce3612b1d84057ccbd045ed19b8ff850a66
Andrea Lemmo <75537622+AndreaLemmo@users.noreply.github.com>
2021-02-12 21:00:43 +0100

    Refactort codice ManagerConnessioneDB

--- iPlanner/src/dbManager/ManagerConnessioneDB.java
+++ iPlanner/src/dbManager/ManagerConnessioneDB.java
@@ -14,70 +14,44 @@ import java.sql.Statement;
 
 import javax.swing.JOptionPane;
 
-import org.postgresql.util.PSQLException;
 
 public class ManagerConnessioneDB {
+	private static ManagerConnessioneDB instance;
 	
-	//ATTRIBUTI
+	private Connection connection = null;
 	
-	private static ManagerConnessioneDB instance;	//istanza singola della classe
+	private final String username = "postgres";
+	private final String password = "160995";
+	private final String nomeDB = "iplanner";
+	private final String url = "jdbc:postgresql://localhost:5432/";
 	
-	private Connection connection = null;	//connessione al DB
-	
-	private final String username = "postgres";	//username per accesso al DB
-	private final String password = "michele"; //password per accesso al DB
-	private final String nomeDB = "MikeDB4";	//nome del database
-	private final String url = "jdbc:postgresql://localhost:5432/";	//url per accedere al DB su AWS
-	
-	//METODI
-	
-	//Costruttore del ManagerConnessioneDB
-	/*Costruttore privato che può essere usato solo qui quando viene definita una sua istanza.
-	*Quando viene costruito il ManagerConnessioneDB viene automaticamente creata anche una
-	*connessione (connection:Connection) al DB. Se la connessione fallisce, restituisce un'eccezione
-	*(SQLException).*/
-	private ManagerConnessioneDB() throws SQLException
-	{
-		try 
-		{
+	private ManagerConnessioneDB() throws SQLException {
+		try {
 			Class.forName("org.postgresql.Driver");
-			connection = DriverManager.getConnection(url,username,password);	//stabilisce la connessione al DB
-			creaDatabase(nomeDB);	//crea il database se non esiste
-			connection = DriverManager.getConnection(url+nomeDB,username,password);	//si connette al DB creato/esistente
-		}
-		catch(ClassNotFoundException ex) 
-		{
-           	//errore classe driver not found
+			connection = DriverManager.getConnection(url,username,password);
+			creaDatabase(nomeDB);
+			connection = DriverManager.getConnection(url+nomeDB,username,password);
+		} catch(ClassNotFoundException ex) {
         	JOptionPane.showMessageDialog(null,
         			"Impossibile stabilire una connessione al database a causa dei driver.\nContattare uno sviluppatore.",
         			"Errore Connessione Database",
         			JOptionPane.ERROR_MESSAGE);
         }
 	}
-	
-	//Metodo getter della connessione GetConnection(): Connection
-	/*Restituisce semplicemente la connessione al DB creata dal ManagerConnessioneDB.*/
-	public Connection getConnection()
-	{
+
+	public Connection getConnection() {
 		return connection;
 	}
 	
-	//Metodo getter dell'istanza del ManagerConnessioneDB GetInstance(): ManagerConnessioneDB.
-	/*Metodo static e public. Quando viene chiamato restituisce l'istanza del ManagerConnessioneDB
-	*(e quindi volendo la connessione creata al DB) se l'istanza già esiste e la connessione è aperta,
-	*altrimenti la definisce sul posto.*/
-	public static ManagerConnessioneDB getInstance() throws SQLException
-	{
-		if (instance==null)	//se l'istanza non esiste ne crea una (e stabilisce una connessione al DB)
+	public static ManagerConnessioneDB getInstance() throws SQLException {
+		if (instance==null)
 			instance = new ManagerConnessioneDB();
-		else
-			if (instance.getConnection().isClosed())	//se l'istanza esiste ma la connessione è chiusa la riapre creando una nuova istanza
-				instance = new ManagerConnessioneDB();
+		else if (instance.getConnection().isClosed())
+			instance = new ManagerConnessioneDB();
 		
 		return instance;
 	}
 	
-	//Metodo che crea il database nel caso non esista già
     private void creaDatabase(String nomeDB) throws SQLException {
     	try {
 	        Statement statement = null;
