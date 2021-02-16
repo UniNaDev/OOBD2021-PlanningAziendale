51fb236a5fcf333a0a745669bc7327831e17cc22
Andrea Lemmo <75537622+AndreaLemmo@users.noreply.github.com>
2021-02-16 17:05:23 +0100

    Refactor codice Login
    -centrato layout
    -gestita eccezione login correttamente

--- iPlanner/src/controller/dipendente/ControllerAccesso.java
+++ iPlanner/src/controller/dipendente/ControllerAccesso.java
@@ -19,53 +19,55 @@ import interfacceDAO.SalaRiunioneDAO;
 import interfacceDAO.SkillDAO;
 
 public class ControllerAccesso {
-	private Login loginFrame;
+    private Login loginFrame;
 
-	private LuogoNascitaDAO luogoDAO = null;
-	private DipendenteDAO dipDAO = null;
-	private ProgettoDAO projDAO = null;
-	private MeetingDAO meetDAO = null;
-	private SkillDAO skillDAO = null;
-	private SalaRiunioneDAO salaDAO = null;
-	private AmbitoProgettoDAO ambitoDAO = null;
-	
-	private boolean isSegreteria = false;
-	
-	private Dipendente dipendenteLogged = null;
+    private LuogoNascitaDAO luogoDAO = null;
+    private DipendenteDAO dipDAO = null;
+    private ProgettoDAO projDAO = null;
+    private MeetingDAO meetDAO = null;
+    private SkillDAO skillDAO = null;
+    private SalaRiunioneDAO salaDAO = null;
+    private AmbitoProgettoDAO ambitoDAO = null;
 
-	public ControllerAccesso(LuogoNascitaDAO luogoDAO, DipendenteDAO dipDAO, ProgettoDAO projDAO, MeetingDAO meetDAO, SkillDAO skillDAO, SalaRiunioneDAO salaDAO, AmbitoProgettoDAO ambitoDAO, boolean isSegreteria){	
-		this.luogoDAO = luogoDAO;
-		this.dipDAO = dipDAO;
-		this.projDAO = projDAO;
-		this.meetDAO = meetDAO;
-		this.skillDAO = skillDAO;
-		this.salaDAO = salaDAO;
-		this.ambitoDAO = ambitoDAO;
-		
-		this.isSegreteria = isSegreteria;
+    private boolean isSegreteria = false;
 
-		loginFrame=new Login(this);
-		loginFrame.setVisible(true);
-		
+    private Dipendente dipendenteLogged = null;
+
+    public ControllerAccesso(LuogoNascitaDAO luogoDAO, DipendenteDAO dipDAO, ProgettoDAO projDAO, MeetingDAO meetDAO,
+	    SkillDAO skillDAO, SalaRiunioneDAO salaDAO, AmbitoProgettoDAO ambitoDAO, boolean isSegreteria) {
+	this.luogoDAO = luogoDAO;
+	this.dipDAO = dipDAO;
+	this.projDAO = projDAO;
+	this.meetDAO = meetDAO;
+	this.skillDAO = skillDAO;
+	this.salaDAO = salaDAO;
+	this.ambitoDAO = ambitoDAO;
+
+	this.isSegreteria = isSegreteria;
+
+	loginFrame = new Login(this);
+	loginFrame.setVisible(true);
+
+    }
+
+    public void eseguiLogin(String email, String password) throws SQLException {
+	dipendenteLogged = dipDAO.getLoggedDipendente(email, password);
+	try {
+	    dipendenteLogged.setSkills(skillDAO.getSkillsDipendente(dipendenteLogged.getCf()));
+	} catch (SQLException e) {
+	    JOptionPane.showMessageDialog(null,
+		    e.getMessage()
+		    + "\nVerificare che il programma sia aggiornato\noppure contattare uno sviluppatore.",
+		    "Errore #" + e.getSQLState(), JOptionPane.ERROR_MESSAGE);
 	}
+	loginFrame.setVisible(false);
+	ControllerGestioneProfilo controller = new ControllerGestioneProfilo(luogoDAO, dipDAO, projDAO, meetDAO,
+		skillDAO, salaDAO, ambitoDAO, dipendenteLogged);
+    }
 
-	public void eseguiLogin(String email, String password) throws SQLException {
-		dipendenteLogged = dipDAO.getLoggedDipendente(email, password);
-		try {
-			dipendenteLogged.setSkills(skillDAO.getSkillsDipendente(dipendenteLogged.getCf()));
-		} catch(SQLException e) {
-			JOptionPane.showMessageDialog(null,
-					"Impossibile ottenere le skill del dipendente dal database.\nControllare che la connessione al database sia stabilita.",
-					"Errore Interrogazione Database",
-					JOptionPane.ERROR_MESSAGE);
-		}
-		loginFrame.setVisible(false);
-		ControllerGestioneProfilo controller = new ControllerGestioneProfilo(luogoDAO,dipDAO,projDAO,meetDAO,skillDAO,salaDAO,ambitoDAO,dipendenteLogged);
-	}
-
-	public void tornaAIPlanner() {
-		loginFrame.setVisible(false);
-		ControllerStart controller = new ControllerStart(luogoDAO, dipDAO, projDAO, meetDAO, skillDAO, salaDAO, ambitoDAO, isSegreteria);
-	}
-	
+    public void tornaAIPlanner() {
+	loginFrame.setVisible(false);
+	ControllerStart controller = new ControllerStart(luogoDAO, dipDAO, projDAO, meetDAO, skillDAO, salaDAO,
+		ambitoDAO, isSegreteria);
+    }
 }
--- iPlanner/src/gui/dipendente/Login.java
+++ iPlanner/src/gui/dipendente/Login.java
@@ -1,5 +1,9 @@
+/*Finestra di login per il dipendente.
+ *Permette al dipendente di effettuare l'accesso con le sue credenziali: email e password. 
+ ****************************************************************************************/
+
 package gui.dipendente;
-import controller.*;
+
 import controller.dipendente.ControllerAccesso;
 
 import javax.swing.JFrame;
@@ -28,22 +32,14 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class Login extends JFrame {
-
-	//ATTRIBUT
-	//-----------------------------------------------------------------
-	
-	//Attributi GUI
 	private JPanel contentPane;
-	private JTextField emailTextField;	//text field per l'email
-	private JPasswordField passwordField;	//password field per la password
-	private JButton accessoButton;	//pulsante di login
-	private JButton annullaButton;	//pulsante annulla
-	private JLabel mostraPasswordLabel;	//label clickabile per mostrare la password
-	private JLabel emailLabel, passwordLabel;	//label emai e password
+	private JTextField emailTextField;
+	private JPasswordField passwordField;
+	private JButton accessoButton;
+	private JButton annullaButton;
+	private JLabel mostraPasswordLabel;
+	private JLabel emailLabel, passwordLabel;
 
-	//Creazione frame
-	//-----------------------------------------------------------------
-	
 	public Login(ControllerAccesso controller) {
 		setResizable(false);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource("/Icone/WindowIcon_16.png")));
@@ -56,67 +52,55 @@ public class Login extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		
-		//Label "Email"
 		emailLabel = new JLabel("Email");
-		emailLabel.setBounds(300, 120, 57, 14);
+		emailLabel.setBounds(298, 116, 57, 14);
 		emailLabel.setFont(new Font("Consolas", Font.PLAIN, 13));
 		emailLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		//Label "Password"
 		passwordLabel = new JLabel("Password");
-		passwordLabel.setBounds(301, 182, 70, 14);
+		passwordLabel.setBounds(299, 182, 70, 14);
 		passwordLabel.setFont(new Font("Consolas", Font.PLAIN, 13));
 		passwordLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		//Text Field per l'email
 		emailTextField = new JTextField();
 		emailTextField.setFont(new Font("Consolas", Font.PLAIN, 11));
 		emailTextField.setBorder(new MatteBorder(1, 1, 1, 1, (Color) Color.LIGHT_GRAY));
-		emailTextField.setBounds(285, 133, 173, 26);
+		emailTextField.setBounds(280, 133, 173, 26);
 		emailTextField.setHorizontalAlignment(SwingConstants.LEFT);
 		emailTextField.setColumns(10);
-		//Eventi connessi al Text Field dell'email
 		emailTextField.addKeyListener(new KeyAdapter() {
-			//Premuto Enter
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER)
-					login(controller);	//tenta il login
+					login(controller);
 			}
 		});
 		
-		//Password Field per la password
 		passwordField = new JPasswordField();
 		passwordField.setFont(new Font("Consolas", Font.PLAIN, 11));
 		passwordField.setEchoChar('*');
 		passwordField.setBorder(new MatteBorder(1, 1, 1, 1, (Color) Color.LIGHT_GRAY));
-		passwordField.setBounds(285, 193, 173, 26);
+		passwordField.setBounds(280, 193, 173, 26);
 		passwordField.setHorizontalAlignment(SwingConstants.LEFT);
-		//Eventi connessi al Password Field della password
 		passwordField.addKeyListener(new KeyAdapter() {
-			//Premuto Enter
 			@Override
 			public void keyPressed(KeyEvent e) {
 				if (e.getKeyCode() == KeyEvent.VK_ENTER)
-					login(controller);	//tenta il login
+					login(controller);
 			}
 		});
 		
-		
-		//Icona login
 		JLabel iconaLoginLabel = new JLabel("Login");
-		iconaLoginLabel.setBounds(289, 5, 165, 88);
+		iconaLoginLabel.setBounds(284, 5, 165, 88);
 		iconaLoginLabel.setFont(new Font("Consolas", Font.PLAIN, 30));
 		iconaLoginLabel.setIcon(new ImageIcon(Login.class.getResource("/Icone/employee_64.png")));
 		
-		//Icona email
 		JLabel iconaEmailLabel = new JLabel("");
-		iconaEmailLabel.setBounds(286, 108, 16, 26);
+		iconaEmailLabel.setBounds(280, 104, 16, 26);
 		iconaEmailLabel.setIcon(new ImageIcon(Login.class.getResource("/Icone/username_16.png")));
 		
-		//Icona password
 		JLabel iconaPasswordLabel = new JLabel("");
-		iconaPasswordLabel.setBounds(286, 170, 16, 26);
+		iconaPasswordLabel.setBounds(280, 170, 16, 26);
 		iconaPasswordLabel.setIcon(new ImageIcon(Login.class.getResource("/Icone/password_16.png")));
 		contentPane.setLayout(null);
 		contentPane.add(iconaLoginLabel);
@@ -127,27 +111,22 @@ public class Login extends JFrame {
 		contentPane.add(passwordField);
 		contentPane.add(passwordLabel);
 	
-		//Button "Annulla"
 		annullaButton = new JButton("Annulla");
-		//Eventi annessi al button "Annulla"
 		annullaButton.addMouseListener(new MouseAdapter() {
-			//mouse sopra il pulsante
 			@Override
 			public void mouseEntered(MouseEvent e) 
 			{
-				annullaButton.setBackground(Color.LIGHT_GRAY);	//evidenzia il pulsante
+				annullaButton.setBackground(Color.LIGHT_GRAY);
 			}
-			//mouse fuori dal pulsante
 			@Override
 			public void mouseExited(MouseEvent e) 
 			{
-				annullaButton.setBackground(Color.WHITE);	//smette di evidenziarlo
+				annullaButton.setBackground(Color.WHITE);
 			}
 		});
 		annullaButton.addActionListener(new ActionListener() {
-			//click del pulsante
 			public void actionPerformed(ActionEvent e) {
-				controller.tornaAIPlanner();	//annulla tutto e torna alla finestra iniziale di scelta
+				controller.tornaAIPlanner();
 			}
 		});
 		annullaButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
@@ -157,49 +136,43 @@ public class Login extends JFrame {
 		annullaButton.setBounds(10, 364, 121, 26);
 		contentPane.add(annullaButton);
 		
-		//Button per eseguire il login
 		accessoButton = new JButton("Login");
-		//Eventi connessi al button "Login"
 		accessoButton.addMouseListener(new MouseAdapter() {
-			//mouse sopra il pulsante
 			@Override
 			public void mouseEntered(MouseEvent e) 
 			{
-				accessoButton.setBackground(Color.LIGHT_GRAY);	//evidenzia il pulsante
+				accessoButton.setBackground(Color.LIGHT_GRAY);
 			}
-			//mouse fuori dal pulsante
 			@Override
 			public void mouseExited(MouseEvent e) 
 			{
-				accessoButton.setBackground(Color.WHITE);	//smette di evidenziarlo
+				accessoButton.setBackground(Color.WHITE);
 			}
 		});
-		//click del pulsante
 		accessoButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				login(controller);	//tenta di eseguire il login con le credenziali inserite
+				login(controller);
 			}
 		});
 		accessoButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		accessoButton.setBorder(new MatteBorder(1, 1, 1, 1, (Color) Color.LIGHT_GRAY));
-		accessoButton.setBounds(311, 267, 121, 26);
+		accessoButton.setBounds(306, 267, 121, 26);
 		accessoButton.setFont(new Font("Consolas", Font.PLAIN, 13));
 		accessoButton.setBackground(new Color(255, 255, 255));
 		contentPane.add(accessoButton);
 		
-		//Icona mostra password
 		mostraPasswordLabel = new JLabel("");
 		mostraPasswordLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 		mostraPasswordLabel.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) 
 			{
-				passwordField.setEchoChar((char)0); //alla pressione del mouse mostra i caratteri non nascosti
+				passwordField.setEchoChar((char)0);
 			}
 			@Override
 			public void mouseReleased(MouseEvent e) 
 			{
-				passwordField.setEchoChar('*');	//quando si rilascia il mouse mostra di nuovo i caratteri nascosti
+				passwordField.setEchoChar('*');
 			}
 		});
 		mostraPasswordLabel.setIcon(new ImageIcon(Login.class.getResource("/icone/showpass_24.png")));
@@ -209,51 +182,39 @@ public class Login extends JFrame {
 
 	//Altri metodi
 	//-----------------------------------------------------------------
-	
-	//Metodo che tenta di fare il login richiamando il controller
 	private void login(ControllerAccesso controller) {
-		//resetta i colori delle label
 		emailLabel.setForeground(Color.BLACK);
 		passwordLabel.setForeground(Color.BLACK);
 		
-		//se uno dei due campi email e password è vuoto
 		if (emailTextField.getText().isBlank() || passwordField.getText().isBlank()) {
-			//se il text field dell'email è vuoto
 			if (emailTextField.getText().isBlank()) {
-				emailLabel.setForeground(Color.RED);	//rende rossa la label
-				svuotaCampi();	//svuota i campi
+				emailLabel.setForeground(Color.RED);
+				pulisciCampi();
 			}
-			//se il password field della password è vuoto
 			if (passwordField.getText().isBlank()) {
-				passwordLabel.setForeground(Color.RED);	//rende rossa la label
-				svuotaCampi();	//svuota i campi
+				passwordLabel.setForeground(Color.RED);
+				pulisciCampi();
 			}
 			JOptionPane.showMessageDialog(null,
 					"Inserire Email e Password per login.",
 					"Credenziali Vuote",
-					JOptionPane.OK_OPTION);	//mostra un errore
+					JOptionPane.OK_OPTION);
 		}
-		//se invece i campi sono pieni
 		else {
-			//ottiene email e password inseriti
 			String email = emailTextField.getText();
 			String password = passwordField.getText();
-			
 			try {
-				//verifica le credenziali e tenta di effettuare il login
 				controller.eseguiLogin(emailTextField.getText(),passwordField.getText());
 			} catch (SQLException e1) {
 				JOptionPane.showMessageDialog(null,
-						"Credenziali errate oppure connessione fallita al database.",
-						"Errore Interrogazione Database",
-						JOptionPane.ERROR_MESSAGE);	//finestra di errore
-				svuotaCampi();
+						"Credenziali errate.",
+						"Login Fallito",
+						JOptionPane.ERROR_MESSAGE);
 			}
 		}
 	}
 	
-	//Metodo che svuota i campi di email e password
-	private void svuotaCampi() {
+	private void pulisciCampi() {
 		emailTextField.setText("");
 		passwordField.setText("");
 	}
