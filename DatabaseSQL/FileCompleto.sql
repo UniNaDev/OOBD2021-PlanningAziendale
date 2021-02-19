365e14d03c9a036fbf148695cf5bdfe429a3ae9b
Andrea Lemmo <75537622+AndreaLemmo@users.noreply.github.com>
2021-02-19 09:06:40 +0100

    Creato trigger capienza per update sale

--- DatabaseSQL/FileCompleto.sql
+++ DatabaseSQL/FileCompleto.sql
@@ -429,6 +429,49 @@ FOR EACH ROW
 EXECUTE PROCEDURE check_capienza_meeting();
 -----------------------------------------------------------------------------
 
+/*TRIGGER PER CAPIENZA DELLE SALE RISPETTATE (SALA RIUNIONE)
+**Ad ogni update in SalaRiunione controlla che
+**il numero di invitati in ciascun meeting che ospita sia minore o uguale
+**alla capienza della sala in cui avviene.
+**Nel caso evoca un'eccezione e non autorizza l'operazione.
+*********************************************************************/
+
+--FUNCTION
+CREATE OR REPLACE FUNCTION check_capienza_sala() RETURNS TRIGGER
+LANGUAGE PLPGSQL
+AS $$
+DECLARE
+OLDMeeting RECORD;
+BEGIN
+--Per ciascun meeting con codice sala pari a quello della sala modificato
+FOR OLDMeeting IN
+	SELECT *
+	FROM Meeting AS m
+	WHERE m.CodSala = NEW.CodSala
+LOOP
+	--Controlla che il numero di partecipanti in presenza del meeting non superi la capienza della sala
+	IF ((SELECT COUNT(p.CF)
+	   FROM Presenza AS p
+	   WHERE p.IDMeeting = OLDMeeting.IDMeeting
+	   GROUP BY p.IDMeeting) > NEW.Capienza) THEN
+	   		RAISE EXCEPTION 'Il numero di invitati al meeting supera la capienza (%) della sala stabilita.', NEW.Capienza
+			USING
+				HINT = 'Si consiglia di cambiare la sala del meeting o di rimuovere qualche partecipante.',
+				ERRCODE = 'P0002';
+			RETURN OLD;
+	END IF;
+END LOOP;
+RETURN NEW;
+END;
+$$;
+----------------------------------------------------------------------------------------------------------------
+
+--TRIGGER
+CREATE TRIGGER capienza_rispettata_sala BEFORE UPDATE ON SalaRiunione
+FOR EACH ROW
+EXECUTE PROCEDURE check_capienza_sala();
+--------------------------------------------------------------------------------
+
 /*TRIGGER PER ELIMINAZIONE SKILL NON NECESSARIE
 **Quando avviene un delete nella tabella Ablità (quindi un dipendente perde una skill)
 **controlla se la skill appartiene ancora a qualche dipendente altrimenti eliminala
